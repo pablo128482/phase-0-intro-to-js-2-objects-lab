9b9a911adc784ec9ea7415317b4729233b6c656d
pablo128482 <ashley.c.clay@gmail.com>
2023-05-28 21:53:07 -0700

    commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+let emplyoyee = "Sam"
+
+var employee = {
+  name : "Sam",
+ streetAddress : "11 Broadway",
+ };
+
+ function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+
+function destructivelyUpdateEmployeeWithKeyValue(employee, key, value) {
+
+  employee[key] = value;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  let { [key]: deleted, ...newEmployee } = employee;
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key]
+  return employee
+}
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) { 
+    employee[key] = value;
+    return employee;
+  }
+  
